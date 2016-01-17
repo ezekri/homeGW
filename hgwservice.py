873bfcec3feb64a2194871d6f4ba935f95758d7b
ezekri <hexan79@gmail.com>
2016-01-17 22:19:36 +0100

    add useful comments

--- hgwservice.py
+++ hgwservice.py
@@ -2,6 +2,9 @@
 
 from hgwengine import *
 
+#wake up HCL device every 1800 seconds
+#check HCL armed variable and syncronize homeGW armed status every 60 seconds
+#iterate main polling function every second
 poll = Polling(1800,60,1)
 poll.start()
 
