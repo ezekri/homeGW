133bda82813b55189ba61c089225e16266f55bfa
ezekri <hexan79@gmail.com>
2016-01-17 22:06:19 +0100

    add global variables

--- hgwengine.py
+++ hgwengine.py
@@ -7,11 +7,14 @@ import RPi.GPIO as GP
 import json
 
 
-HCL_IP="192.168.1.10"
-HCL_USER="user"
-HCL_PASS="pass"
-
-
+HCL_IP = "192.168.1.10"
+HCL_USER = "user"
+HCL_PASS = "pass"
+HCL_PIN = "1234"
+HCL_ARMED_VAR = "armed"
+HCL_ALARM_DEVICES = [11,15,17,21,31,44,48]
+HCL_ALL_DEVICES = [5,11,15,17,19,21,31,44,48]
+KNOWN_NFC_TAGS = ["3761E99E", "11FB229E", "4102E39E","F1CC67AE"] 
 
 class Debug:
 	def __init__(self,message):
@@ -29,8 +32,8 @@ class Polling:
 		self.pollInterval = pollInterval
 		self.mifare = nxppy.Mifare()
 		self.arme = 0
-		self.alarmDevices = [11,15,17,21,31,44,48]
-		self.allDevices = [5,11,15,17,19,21,31,44,48]
+		self.alarmDevices = HCL_ALARM_DEVICES
+		self.allDevices = HCL_ALL_DEVICES
 		GP.setwarnings(False)
 		GP.setmode(GP.BOARD)
 		GP.setup(11,GP.OUT)
@@ -62,7 +65,7 @@ class Polling:
 				
 			
 			if sync == self.syncInterval:
-				url = "http://"+HCL_USER+":"+HCL_PASS+"@"+HCL_IP+"/api/globalVariables/arme"
+				url = "http://"+HCL_USER+":"+HCL_PASS+"@"+HCL_IP+"/api/globalVariables/" + HCL_ARMED_VAR
 				resp = self.getUrl(url)
 				if (resp and resp.ok):
 					hcl_arme = int(json.loads(resp.content)['value'])
@@ -77,10 +80,9 @@ class Polling:
 					sync = 0
 				
 			uid = self.mifare.select()
-			if uid in ("3761E99E", "11FB229E", "4102E39E","F1CC67AE") :
+			if uid in KNOWN_NFC_TAGS :
 				Debug("-- Tag from " + uid).write()
-				headers = {"Content-Type" : "application/x-www-form-urlencoded;charset=utf-8"}
-		        	if  self.arme == 0:
+				if  self.arme == 0:
 					for dev in self.alarmDevices:
 						url = "http://"+HCL_USER+":"+HCL_PASS+"@"+HCL_IP+"/api/callAction?deviceID=" + str(dev) + "&name=setArmed&arg1=1"
 						Debug("-- Arming device " + str(dev) + " in progress...").write()
@@ -102,17 +104,17 @@ class Polling:
 						Debug("-- Arming KO").write()
 				else:
 					for dev in self.alarmDevices:
-						url = "http://"+HCL_USER+":"+HCL_PASS+"@"+HCL_IP+"/api/callAction?deviceID=" + str(dev) + "&name=setArmed&arg1=0&arg2=7911"
+						url = "http://"+HCL_USER+":"+HCL_PASS+"@"+HCL_IP+"/api/callAction?deviceID=" + str(dev) + "&name=setArmed&arg1=0&arg2=" + HCL_PIN
 						Debug("-- Disarming device " + str(dev) + " in progress...").write()
 						resp = self.getUrl(url)
-						Debug("-- HCL response status: " (str(resp.status_code) if(resp is not None) else "None")).write()
+						Debug("-- HCL response status: " + (str(resp.status_code) if(resp is not None) else "None")).write()
 						if (resp and resp.ok):
 							Debug("-- Disarming device " + str(dev) + " OK").write()
 						else:
 							Debug("-- Disarming device " + str(dev) + " KO").write()	
 					GP.output(11,False)
 					url = "http://"+HCL_USER+":"+HCL_PASS+"@"+HCL_IP+"/api/sceneControl?id=27&action=start"
-					Debug("-- Sirene OFF").write()
+					Debug("-- Siren OFF").write()
 					resp = self.getUrl(url)
 					self.arme = 0
 					Debug("-- Disarming finished").write()
